5538011c613ab5c5effd2497f8901dcadbd31e1b
WEBDOOT <webdoot.com@gmail.com>
2023-07-16 19:57:33 +0530

    Social media icon added

--- footer.php
+++ footer.php
@@ -4,7 +4,12 @@
             <div class="col-lg-3 col-md-6 mb-5">
                 <h5 class="mb-4 text-white text-uppercase font-weight-bold">Get In Touch</h5>                
                 <p class="font-weight-medium"><i class="fa fa-envelope mr-2"></i>MAIL@INFODOOT.COM</p>
-                <h6 class="mt-4 mb-3 text-white text-uppercase font-weight-bold">Follow Us</h6>                
+                <h6 class="mt-4 mb-3 text-white text-uppercase font-weight-bold">Follow Us</h6> 
+                <div class="d-flex justify-content-start">
+                    <a class="btn btn-lg btn-secondary btn-lg-square mr-2" href="https://www.facebook.com/infodootpage"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-lg btn-secondary btn-lg-square mr-2" href="https://twitter.com/Infodootid"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-lg btn-secondary btn-lg-square mr-2" href="https://instagram.com/infodootpage"><i class="fab fa-instagram"></i></a>
+                </div>            
             </div>
             <div class="col-lg-6 col-md-6 mb-5">
                 <h5 class="mb-4 text-white text-uppercase font-weight-bold">Categories</h5>
@@ -18,6 +23,8 @@
                 <h5 class="mb-4 text-white text-uppercase font-weight-bold">Important Links</h5>
                 <p class="font-weight-medium"><a class="text-reset" href="<?php echo esc_html(get_bloginfo('url')) . '/about'; ?>">About</a></p>
                 <p class="font-weight-medium"><a class="text-reset" href="<?php echo esc_html(get_bloginfo('url')) . '/contact'; ?>">Contact</a></p>
+                <p class="font-weight-medium"><a class="text-reset" href="<?php echo esc_html(get_bloginfo('url')) . '/dmca'; ?>">DMCA</a></p>
+                <p class="font-weight-medium"><a class="text-reset" href="<?php echo esc_html(get_bloginfo('url')) . '/disclaimer'; ?>">Disclaimer</a></p>
                 <p class="font-weight-medium"><a class="text-reset" href="<?php echo esc_html(get_bloginfo('url')) . '/privacy-policy'; ?>">Privacy Policy</a></p>
             </div>
         </div>
